dcb8354d9d8718bf0e59374b0d4eb671679e8fa7
herolya <helia.ghahraman@gmail.com>
2021-05-29 19:02:07 +0430

    added "heli jon"

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,6 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hellooo World!");
+        System.out.println("Heli jon is such a hero:)");
     }
 }
