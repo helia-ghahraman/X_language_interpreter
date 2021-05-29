aa284efd117f08d6e182a56234ee71a0841718fe
Ali <ahamidzadeh93@gmail.com>
2021-05-29 20:56:36 +0430

    add some codes.

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Program {
-    public static void main(String[] args) {
-        System.out.println("Hellooo World!");
-        System.out.println("Heli jon is such a hero:)");
-        System.out.println("Na da");
-        System.out.println("=]");
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("TextFiles//src1.txt");
+        Scanner sc = new Scanner(f);
+        if (f.isDirectory()) {
+            throw new IllegalArgumentException("there is a directory...");
+        } else if (f.isFile()) {
+
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+
+            }
+        }
+
     }
 }
