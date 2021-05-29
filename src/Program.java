a4112c64e1450d9ef27acdceebd1908a4145a1f7
Ali <ahamidzadeh93@gmail.com>
2021-05-29 20:23:25 +0430

    Test

--- src/Program.java
+++ src/Program.java
@@ -2,5 +2,7 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Hellooo World!");
         System.out.println("Heli jon is such a hero:)");
+        System.out.println("Na da");
+
     }
 }
