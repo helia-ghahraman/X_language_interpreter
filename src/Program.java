2e2721882af5c9e3fd155c7d9aa145e582c14e73
Ali <ahamidzadeh93@gmail.com>
2021-06-04 02:01:10 +0430

    fix bugs. dont realize empty line.

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Program {
+
+    static HashMap<String, Integer> intVariables = new HashMap<>();
+    static HashMap<String, Float> floatVariables = new HashMap<>();
+
     public static void main(String[] args) throws FileNotFoundException {
         Boolean faz1 = true; //true -> faz1, false -> faz2
         File f = new File("TextFiles//src1.txt");
@@ -14,13 +19,13 @@ public class Program {
         } else if (f.isFile()) {
             while (faz1) {
                 String line = sc.nextLine();
+                if (line.isEmpty()) {
+                    continue;
+                }
                 String[] tokens = line.split(" ");
                 System.out.println(Arrays.toString(tokens));
                 if (tokens[0].equals("%%")) {
                     faz1 = false; //jump to faz2
-                } else if (Variable.intVariables.containsKey(tokens[1]) || Variable.floatVariables.containsKey(tokens[1])) {
-                    throw new IllegalArgumentException("Duplicate Variables name!");
-
                 } else if (tokens[0].equals("int")) {
                     if (tokens.length == 4) {
                         Variable variable1 = new Variable(tokens[1], Integer.parseInt(tokens[3]));
@@ -40,8 +45,8 @@ public class Program {
             }
 //            while (sc.hasNextLine()){}
         }
-        System.out.println(Variable.intVariables);
-        System.out.println(Variable.floatVariables);
+        System.out.println(intVariables);
+        System.out.println(floatVariables);
 
     }
 }
--- src/Variable.java
+++ src/Variable.java
@@ -1,23 +1,22 @@
 import java.util.HashMap;
 
 public class Variable {
-    static HashMap<String, Integer> intVariables = new HashMap<>();
-    static HashMap<String, Float> floatVariables = new HashMap<>();
+
 
     public Variable(String type, String name) {
-        if (type == "int")
-            intVariables.put(name, 0);
-        else if (type == "float")
-            floatVariables.put(name, (float) 0);
+        if (type.equals("int"))
+            Program.intVariables.put(name, (int) 0);
+        else if (type.equals("float"))
+            Program.floatVariables.put(name, (float) 0.0);
 
     }
 
     public Variable(String name, int amount) {
-        intVariables.put(name, amount);
+        Program.intVariables.put(name, amount);
     }
 
     public Variable(String name, float amount) {
-        floatVariables.put(name, amount);
+        Program.floatVariables.put(name, amount);
     }
 
 }
