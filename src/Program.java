27d689b1b856c5f80da9f7f0c0d742caa67bd91e
Ali <ahamidzadeh93@gmail.com>
2021-06-03 23:09:35 +0430

    add some codes.

--- src/Program.java
+++ src/Program.java
@@ -1,17 +1,16 @@
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Program {
-    static HashMap<String, Integer> intVariables = new HashMap<>();
-    static HashMap<String, Float> floatVariables = new HashMap<>();
-
     public static void main(String[] args) throws FileNotFoundException {
         Boolean faz1 = true; //true -> faz1, false -> faz2
         File f = new File("TextFiles//src1.txt");
         Scanner sc = new Scanner(f);
         if (f.isDirectory()) {
             throw new IllegalArgumentException("there is a directory...");
+
         } else if (f.isFile()) {
             while (faz1) {
                 String line = sc.nextLine();
@@ -19,21 +18,21 @@ public class Program {
                 System.out.println(Arrays.toString(tokens));
                 if (tokens[0].equals("%%")) {
                     faz1 = false; //jump to faz2
-                } else if (intVariables.containsKey(tokens[1]) || floatVariables.containsKey(tokens[1])) {
+                } else if (Variable.intVariables.containsKey(tokens[1]) || Variable.floatVariables.containsKey(tokens[1])) {
                     throw new IllegalArgumentException("Duplicate Variables name!");
+
                 } else if (tokens[0].equals("int")) {
                     if (tokens.length == 4) {
-                        intVariables.put(tokens[1], Integer.parseInt(tokens[3]));
+                        Variable variable1 = new Variable(tokens[1], Integer.parseInt(tokens[3]));
                     } else {
-                        intVariables.put(tokens[1], 0);
+                        Variable variable2 = new Variable(tokens[0], tokens[1]);
                     }
-                } else if (tokens[1].equals("float")) {
+                } else if (tokens[0].equals("float")) {
                     if (tokens.length == 4) {
-                        floatVariables.put(tokens[1], Float.parseFloat(tokens[3]));
+                        Variable variable3 = new Variable(tokens[1], Float.parseFloat(tokens[3]));
                     } else {
-                        floatVariables.put(tokens[1], (float) 0.0);
+                        Variable variable4 = new Variable(tokens[0], tokens[1]);
                     }
-
                 } else {
                     throw new IllegalArgumentException("Illegal Data Type");
                 }
@@ -41,8 +40,8 @@ public class Program {
             }
 //            while (sc.hasNextLine()){}
         }
-        System.out.println(intVariables);
-        System.out.println(floatVariables);
+        System.out.println(Variable.intVariables);
+        System.out.println(Variable.floatVariables);
 
     }
 }
--- src/Variable.java
+++ src/Variable.java
@@ -1,12 +1,23 @@
+import java.util.HashMap;
+
 public class Variable {
+    static HashMap<String, Integer> intVariables = new HashMap<>();
+    static HashMap<String, Float> floatVariables = new HashMap<>();
 
     public Variable(String type, String name) {
+        if (type == "int")
+            intVariables.put(name, 0);
+        else if (type == "float")
+            floatVariables.put(name, (float) 0);
+
     }
 
-    public Variable(String type, String name, int amount) {
+    public Variable(String name, int amount) {
+        intVariables.put(name, amount);
     }
 
-    public Variable(String type, String name, float amount) {
+    public Variable(String name, float amount) {
+        floatVariables.put(name, amount);
     }
 
 }
